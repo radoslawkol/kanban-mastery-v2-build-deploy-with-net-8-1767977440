e963c4f0b6846a916c07ed1eef90a3f8245c96fc
Radosław Kołoczek <85928097+radoslawkol@users.noreply.github.com>
2026-02-01 13:14:13 +0100

    Configure EF Core DbContext and initial migration (#3)
    * chore(deps): add ef core 8 and identity packages
    
    * chore(persistance): add ApplicationDbContext
    
    * chore(persistence): configure board membership mappings
    
    * chore(config): register DbContext and Identity API endpoints
    
    * chore(persistence): add initial migration

---
+++ KanbanAPI/Data/ApplicationDbContext.cs
@@ -0,0 +1,37 @@
+﻿using KanbanAPI.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace KanbanAPI.Data
+{
+	public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+	{
+		public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) 
+			: base(options) 
+		{ 
+		}
+
+		public DbSet<Board> Boards {  get; set; }
+		public DbSet<Column> Columns { get; set; }
+		public DbSet<Card> Cards { get; set; }
+		public DbSet<BoardMember> BoardMembers { get; set; }
+
+		protected override void OnModelCreating(ModelBuilder builder)
+		{
+			base.OnModelCreating(builder);
+
+			builder.Entity<BoardMember>()
+				.HasKey(bm => new { bm.UserId, bm.BoardId });
+
+			builder.Entity<BoardMember>()
+				.HasOne(bm => bm.User)
+				.WithMany(u => u.Boards)
+				.HasForeignKey(bm => bm.UserId);
+
+			builder.Entity<BoardMember>()
+				.HasOne(bm => bm.Board)
+				.WithMany(b => b.Members)
+				.HasForeignKey(bm => bm.BoardId);
+		}
+	}
+}
