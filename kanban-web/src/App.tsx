6806fd58536390cfe7a027d978b6c5070b15b67b
Radosław Kołoczek <radoslaw.koloczek@gmail.com>
2026-03-25 19:11:57 +0100

    feat: implement routing for dashboard and registration pages

--- kanban-web/src/App.tsx
+++ kanban-web/src/App.tsx
@@ -1,7 +1,16 @@
+import { Navigate, Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage";
+import RegisterPage from "./pages/RegisterPage";
 
 function App() {
-	return <HomePage />;
+	return (
+		<Routes>
+			<Route path='/' element={<Navigate to='/dashboard' replace />} />
+			<Route path='/dashboard' element={<HomePage />} />
+			<Route path='/register' element={<RegisterPage />} />
+			<Route path='*' element={<Navigate to='/dashboard' replace />} />
+		</Routes>
+	);
 }
 
 export default App;
