20284e7e35f107dea73dfb683d13b00a9cfab4f1
Radosław Kołoczek <radoslaw.koloczek@gmail.com>
2026-03-31 21:17:44 +0200

    feat: implement ProtectedRoute component for route protection

--- kanban-web/src/App.tsx
+++ kanban-web/src/App.tsx
@@ -2,12 +2,20 @@ import { Navigate, Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import RegisterPage from "./pages/RegisterPage";
 import LoginPage from "./pages/LoginPage";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 function App() {
 	return (
 		<Routes>
 			<Route path='/' element={<Navigate to='/dashboard' replace />} />
-			<Route path='/dashboard' element={<HomePage />} />
+			<Route
+				path='/dashboard'
+				element={
+					<ProtectedRoute>
+						<HomePage />
+					</ProtectedRoute>
+				}
+			/>
 			<Route path='/register' element={<RegisterPage />} />
 			<Route path='/login' element={<LoginPage />} />
 			<Route path='*' element={<Navigate to='/dashboard' replace />} />
